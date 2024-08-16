009e5d97e353f13cfd8e6e5bf460f038bf55d2a9
Jacob Sides <sides798@gmail.com>
2024-08-16 19:30:40 -0400

    init

--- app/remotion/constants.ts
+++ app/remotion/constants.ts
@@ -1,10 +1,10 @@
 import { VERSION } from 'remotion';
 
 export const COMPOSITION_FPS = 30;
-export const COMPOSITION_DURATION_IN_FRAMES = 7 * COMPOSITION_FPS;
+export const COMPOSITION_DURATION_IN_FRAMES = 12 * COMPOSITION_FPS;
 export const COMPOSITION_WIDTH = 1920;
 export const COMPOSITION_HEIGHT = 1080;
-export const COMPOSITION_ID = 'LogoAnimation';
+export const COMPOSITION_ID = 'NextDocsOnPages';
 export const RAM = 2048;
 export const DISK = 2048;
 export const TIMEOUT = 240;
--- app/remotion/logo-animation.tsx
+++ app/remotion/logo-animation.tsx
@@ -1,90 +1,62 @@
 import { useEffect } from 'react';
 import {
 	AbsoluteFill,
+	Audio,
 	interpolate,
-	Sequence,
-	Series,
 	spring,
+	staticFile,
 	useCurrentFrame,
 	useVideoConfig,
 } from 'remotion';
-import { LogoAnimationSequence } from './components/logo-animation-sequence';
-import { PersonalizedName } from './components/personalized-name';
-import { PlusSymbol } from './components/plus-symbol';
-import { RemixLineToPerson } from './components/remix-logo/remix-line-to-person';
-import { RemixNotAnimated } from './components/remix-logo/remix-not-animated';
-import { RemixPersonToFusion } from './components/remix-logo/remix-person-to-fusion';
-import { RemotionLineToPerson } from './components/remotion-logo/remotion-line-to-person';
-import { RemotionNotAnimated } from './components/remotion-logo/remotion-not-animated';
-import { RemotionPersonToFusion } from './components/remotion-logo/remotion-person-to-fusion';
-import type { LogoAnimationProps } from './constants';
 import { loadFonts } from './load-fonts';
 
-export const LogoAnimation: React.FC<LogoAnimationProps> = (
-	logoAnimationProps
-) => {
-	const { fps } = useVideoConfig();
+export const LogoAnimation: React.FC = () => {
+	const { fps, durationInFrames: originalDurationInFrames } = useVideoConfig();
 	const frame = useCurrentFrame();
-	const { personalizedName } = logoAnimationProps;
+	const delayInFrames = 1 * fps;
+	const extendedDurationInFrames = originalDurationInFrames + 3 * fps; // Extend duration by 3 seconds
+
 	useEffect(() => {
 		loadFonts();
 	}, []);
 
 	const progress = spring({
 		fps,
-		frame,
+		frame: frame - delayInFrames,
 		config: {
 			damping: 200,
 		},
+		durationInFrames: originalDurationInFrames - delayInFrames // Maintain original spring duration
 	});
 
-	const remixXOffset = interpolate(progress, [0, 1], [-900, 180]);
-	const remotionXOffset = interpolate(progress, [0, 1], [900, 0]);
-	const plusYOffset = interpolate(progress, [0, 1], [900, 0]);
+	const scale = interpolate(progress, [0, 1], [1, 3]);
+	const translateX = interpolate(progress, [0, 1], [0, 0]);
+	const translateY = interpolate(progress, [0, 1], [0, 0]);
 
-	const opacity = spring({
-		fps,
-		frame: frame - fps * 1.5,
-		config: {
-			damping: 200,
-		},
-	});
+	const song = staticFile("/song.mp3");
+	const docImage = staticFile("/NextHeadDocs.png");
 
 	return (
 		<AbsoluteFill
 			style={{
 				backgroundColor: 'white',
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				overflow: 'hidden', // Prevents the image from overflowing the bounding box
 			}}
 		>
-			<Series>
-				<Series.Sequence durationInFrames={fps * 1.5}>
-					<PersonalizedName personalizedName={personalizedName} />
-				</Series.Sequence>
-				<Series.Sequence durationInFrames={fps * 1.5}>
-					<AbsoluteFill
-						style={{
-							opacity,
-						}}
-					>
-						<RemixNotAnimated horizontalOffset={remixXOffset} />
-						<RemotionNotAnimated horizontalOffset={remotionXOffset} />
-						<PlusSymbol verticalOffset={plusYOffset} />
-					</AbsoluteFill>
-				</Series.Sequence>
-				<Series.Sequence durationInFrames={fps * 1.5}>
-					<RemixLineToPerson horizontalOffset={remixXOffset} />
-					<RemotionLineToPerson horizontalOffset={remotionXOffset} />
-					<PlusSymbol />
-				</Series.Sequence>
-				<Series.Sequence durationInFrames={fps * 1.5}>
-					<RemixPersonToFusion horizontalOffset={remixXOffset} />
-					<RemotionPersonToFusion horizontalOffset={remotionXOffset} />
-					<PlusSymbol />
-				</Series.Sequence>
-			</Series>
-			<Sequence from={fps * 5.5} durationInFrames={fps * 4}>
-				<LogoAnimationSequence />
-			</Sequence>
+			<Audio startFrom={1.5 * fps} src={song} />
+			<img
+				src={docImage}
+				style={{
+					width: '100%',
+					height: 'auto',
+					transform: `scale(${scale}) translate(${translateX}px, ${translateY}px)`,
+					transformOrigin: 'top left', // Adjust origin point
+					willChange: 'transform'  // Performance optimization
+				}}
+			/>
 		</AbsoluteFill>
 	);
 };
